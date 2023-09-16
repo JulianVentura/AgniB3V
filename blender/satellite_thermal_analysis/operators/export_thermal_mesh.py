65da715847b547fbffd8e57fb19f758c7faf92c5
Franco <fbarreneche@fi.uba.ar>
2023-09-16 16:47:14 -0300

    Add thermal mesh export operator

--- blender/satellite_thermal_analysis/operators/export_thermal_mesh.py
+++ blender/satellite_thermal_analysis/operators/export_thermal_mesh.py
@@ -1,9 +1,21 @@
 import bpy
+import bmesh
+import os
 
 class ExportThermalMesh(bpy.types.Operator):
     bl_idname = "thermal.export_mesh"
     bl_label = "Exportar malla térmica"
 
     def execute(self, context):
-        print("Hello World")
-        return {'FINISHED'}
+        obj = bpy.context.active_object
+        f = open(os.environ["HOME"] + "/" + obj.name + ".csv", "w")
+        bpy.ops.object.mode_set(mode = 'EDIT')
+        bm = bmesh.from_edit_mesh(obj.data)
+        for v in bm.verts:
+            neighbors = []
+            for edge in v.link_edges:
+                for neighbour_v in edge.verts:
+                    if neighbour_v.index != v.index:
+                        neighbors.append(neighbour_v.index)
+                        f.write(",".join(map(str, [v.index, v.co.x, v.co.y, v.co.z] + neighbors)) + "\n")
+                        return {'FINISHED'}

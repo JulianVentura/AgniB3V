87fe411ef00d28407c43f460d6437154398fc27b
Franco <fbarreneche@fi.uba.ar>
2023-09-17 16:55:00 -0300

    Add default tool settings and tidy ui

--- blender/satellite_thermal_analysis/property_groups/tool_settings.py
+++ blender/satellite_thermal_analysis/property_groups/tool_settings.py
@@ -1,5 +1,5 @@
 import bpy
 
 class ThermalToolSettings(bpy.types.PropertyGroup):
-    solar_direction_vector : bpy.props.FloatVectorProperty()
-    ray_cast_displacement : bpy.props.FloatProperty()
+    solar_direction_vector : bpy.props.FloatVectorProperty(name="Solar Direction Vector", default=(0.0,0.0,-1.0))
+    ray_cast_displacement : bpy.props.FloatProperty(name="Desplazamiento de Raycast", default=0.01, min=0.001, max=2)
--- blender/satellite_thermal_analysis/ui/panels.py
+++ blender/satellite_thermal_analysis/ui/panels.py
@@ -11,16 +11,11 @@ class ThermalAnalysisPanel(bpy.types.Panel):
         layout = self.layout
         obj = context.object
 
-        row = layout.row()
-        row.prop(context.scene.thermal_tool_settings, "solar_direction_vector")
-        row.prop(context.scene.thermal_tool_settings, "ray_cast_displacement")
-        
-        row = layout.row()
-        row.operator("thermal.export_mesh")
-        
-        row = layout.row()
-        calc_solar_view_factor = row.operator("thermal.calc_solar_view_factor")
-        #calc_solar_view_factor.solar_direction_vector = [0,0,-1]
-        #calc_solar_view_factor.ray_cast_displacement = 0.01
-
-
+        column = layout.column()
+        column.label(text="Configuración")
+        column.prop(context.scene.thermal_tool_settings, "solar_direction_vector")
+        column.prop(context.scene.thermal_tool_settings, "ray_cast_displacement", text="Desplazamiento", slider=True)
+        column.label(text="Malla")
+        column.operator("thermal.export_mesh", text="Exportar")
+        column.label(text="Factor de vista solar")
+        column.operator("thermal.calc_solar_view_factor", text="Calcular")

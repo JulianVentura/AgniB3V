19555209dffa2fb432c06f38269e5ac0a29c8d2d
Franco <fbarreneche@fi.uba.ar>
2023-09-20 16:30:29 -0300

    Add sum of portion of faces area to vertices

--- blender/satellite_thermal_analysis/operators/export_thermal_mesh.py
+++ blender/satellite_thermal_analysis/operators/export_thermal_mesh.py
@@ -8,14 +8,21 @@ class ExportThermalMesh(bpy.types.Operator):
 
     def execute(self, context):
         obj = bpy.context.active_object
-        f = open(os.environ["HOME"] + "/" + obj.name + ".csv", "w")
+        file = open(os.environ["HOME"] + "/" + obj.name + ".csv", "w")
         bpy.ops.object.mode_set(mode = 'EDIT')
         bm = bmesh.from_edit_mesh(obj.data)
+        weights = {}
+        for f in obj.data.polygons:
+            for v in f.vertices:
+                if not v in weights:
+                    weights[v] = 0
+                weights[v] += f.area / len(f.vertices)
+
         for v in bm.verts:
             neighbors = []
             for edge in v.link_edges:
                 for neighbour_v in edge.verts:
                     if neighbour_v.index != v.index:
                         neighbors.append(neighbour_v.index)
-            f.write(",".join(map(str, [v.index, v.co.x, v.co.y, v.co.z] + neighbors)) + "\n")
+            file.write(",".join(map(str, [v.index, v.co.x, v.co.y, v.co.z] + [weights[v.index]] + neighbors)) + "\n")
         return {'FINISHED'}

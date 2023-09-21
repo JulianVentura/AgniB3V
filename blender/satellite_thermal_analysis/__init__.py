93c8a045250cc9d021fbf59207cc43fa56e163ac
Franco <fbarreneche@fi.uba.ar>
2023-09-21 15:37:52 -0300

    Add load and represent temperature

--- blender/satellite_thermal_analysis/__init__.py
+++ blender/satellite_thermal_analysis/__init__.py
@@ -1,6 +1,6 @@
 import bpy
 from .ui import panels, custom_materials
-from .operators import export_thermal_mesh, calc_solar_view_factor
+from .operators import export_thermal_mesh, calc_solar_view_factor, load_temperature_values, paint_temperature_values
 from .property_groups import tool_settings
 
 bl_info = {
@@ -13,6 +13,8 @@ def register():
     bpy.utils.register_class(tool_settings.ThermalToolSettings)
     bpy.types.Scene.thermal_tool_settings = bpy.props.PointerProperty(type=tool_settings.ThermalToolSettings)
     bpy.utils.register_class(calc_solar_view_factor.CalcSolarViewFactor)
+    bpy.utils.register_class(load_temperature_values.LoadTemperatureValues)
+    bpy.utils.register_class(paint_temperature_values.PaintTemperatureValues)
     bpy.utils.register_class(export_thermal_mesh.ExportThermalMesh)
     bpy.utils.register_class(panels.ThermalAnalysisPanel)
     custom_materials.custom_materials_register()
@@ -20,6 +22,8 @@ def register():
 def unregister():
     bpy.utils.unregister_class(panels.ThermalAnalysisPanel)
     bpy.utils.unregister_class(export_thermal_mesh.ExportThermalMesh)
+    bpy.utils.unregister_class(paint_temperature_values.PaintTemperatureValues)
+    bpy.utils.unregister_class(load_temperature_values.LoadTemperatureValues)
     bpy.utils.unregister_class(calc_solar_view_factor.CalcSolarViewFactor)
     del bpy.types.Scene.thermal_tool_settings
     bpy.utils.unregister_class(tool_settings.ThermalToolSettings)
--- blender/satellite_thermal_analysis/ui/panels.py
+++ blender/satellite_thermal_analysis/ui/panels.py
@@ -19,3 +19,7 @@ class ThermalAnalysisPanel(bpy.types.Panel):
         column.operator("thermal.export_mesh", text="Exportar")
         column.label(text="Factor de vista solar")
         column.operator("thermal.calc_solar_view_factor", text="Calcular")
+        column.label(text="Resultados")
+        column.prop(context.scene.thermal_tool_settings, "mesh_file_path", text="Cargar")
+        column.operator("thermal.load_temperature_values", text="Cargar")
+        column.operator("thermal.paint_temperature_values", text="Pintar")

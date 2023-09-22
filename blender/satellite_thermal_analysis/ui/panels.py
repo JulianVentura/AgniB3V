26b34d3854e3acb8c6ae141e8e65879fb252f659
Franco <fbarreneche@fi.uba.ar>
2023-09-21 21:49:09 -0300

    Add avg temperature of selected vertices

--- blender/satellite_thermal_analysis/ui/panels.py
+++ blender/satellite_thermal_analysis/ui/panels.py
@@ -1,4 +1,5 @@
 import bpy
+import bmesh
 
 class ThermalAnalysisPanel(bpy.types.Panel):
     bl_label = "Analisis Termico"
@@ -20,6 +21,21 @@ class ThermalAnalysisPanel(bpy.types.Panel):
         column.label(text="Factor de vista solar")
         column.operator("thermal.calc_solar_view_factor", text="Calcular")
         column.label(text="Resultados")
-        column.prop(context.scene.thermal_tool_settings, "mesh_file_path", text="Cargar")
+        column.prop(context.scene.thermal_tool_settings, "mesh_file_path", text="")
         column.operator("thermal.load_temperature_values", text="Cargar")
         column.operator("thermal.paint_temperature_values", text="Pintar")
+
+        if (bpy.context.object.mode != 'EDIT'):
+            return
+
+        bm = bmesh.from_edit_mesh(obj.data)
+        temperature_layer = bm.verts.layers.float.active
+        if(temperature_layer):
+            selected_vertices = []
+            for v in bm.verts:
+                if v.select:
+                    selected_vertices.append(v)
+            if len(selected_vertices) == 0:
+                return
+            t_prom = sum(map(lambda v: v[temperature_layer], selected_vertices))/len(selected_vertices)
+            column.label(text="T. promedio: {:10.4f}".format(t_prom))

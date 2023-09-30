f6496eec1a03db927ad1b24b1ab0a61a2ccd777a
Gian <gbelinche@fi.uba.ar>
2023-09-30 17:35:13 -0300

    Fix dot product bug, add examples

--- opencl/src/main.rs
+++ opencl/src/main.rs
@@ -4,9 +4,20 @@ use anyhow::Result;
 use log::error;
 
 fn run() -> Result<()> {
-    // examples::test_square_only_temperature(false)?;
+    examples::test_square_only_temperature(true)?;
+    println!("-------------------");
+    //examples::test_square_only_temperature_rotated_90(true)?;
+    println!("-------------------");
+    examples::test_square_only_temperature_translated(true)?;
+    println!("-------------------");
+    examples::test_square_only_temperature_rotated_45_xy(true)?;
+    println!("-------------------");
+    examples::test_square_only_temperature_rotated_45_xz(true)?;
+    println!("-------------------");
+    examples::test_square_only_temperature_deformated(true)?;
+
     // examples::test_square_only_heat(false)?;
-    examples::test_2d_plane()?;
+    //examples::test_2d_plane()?;
 
     Ok(())
 }

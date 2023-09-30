c3a2d6a59b2160b297b4c17a7001223540305229
Gian <gbelinche@fi.uba.ar>
2023-09-30 19:17:00 -0300

    Add automated tests module

--- opencl/src/main.rs
+++ opencl/src/main.rs
@@ -4,21 +4,6 @@ use anyhow::Result;
 use log::error;
 
 fn run() -> Result<()> {
-    examples::test_square_only_temperature(true)?;
-    println!("-------------------");
-    //examples::test_square_only_temperature_rotated_90(true)?;
-    println!("-------------------");
-    examples::test_square_only_temperature_translated(true)?;
-    println!("-------------------");
-    examples::test_square_only_temperature_rotated_45_xy(true)?;
-    println!("-------------------");
-    examples::test_square_only_temperature_rotated_45_xz(true)?;
-    println!("-------------------");
-    examples::test_square_only_temperature_deformated(true)?;
-
-    // examples::test_square_only_heat(false)?;
-    //examples::test_2d_plane()?;
-
     Ok(())
 }
 

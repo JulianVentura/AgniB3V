66efdbb2ecea945646a155af5443498df9749133
guidobotta <gbotta@fi.uba.ar>
2023-10-13 16:40:10 -0300

    Export mesh in vtk and json with props

--- freecad/commands/CmdExportMesh.py
+++ freecad/commands/CmdExportMesh.py
@@ -1,5 +1,7 @@
 import FreeCAD
 import os
+import json
+import re
 
 class CmdExportMesh:
     def Activated(self):
@@ -18,22 +20,6 @@ class CmdExportMesh:
             FreeCAD.Console.PrintError("No FEMMesh object found\n")
             return
         
-        triangles = {}
-        trianglesByNode = {}
-
-        FreeCAD.Console.PrintMessage("Getting shape mesh object\n")
-        shapeMeshObject = femMeshObject.FemMesh
-        if shapeMeshObject.FaceCount == 0:
-            FreeCAD.Console.PrintError("No faces found in mesh\n")
-            FreeCAD.Console.PrintError("This is probably because mesh was generated with 3D shape\n")
-            return
-        
-        for face in shapeMeshObject.Faces:
-            triangleNodes = shapeMeshObject.getElementNodes(face)
-            triangles[face] = triangleNodes
-            for node in triangleNodes:
-                trianglesByNode[node] = trianglesByNode.get(node, []) + [face]
-        
         FreeCAD.Console.PrintMessage("Getting material objects\n")
         materialObjects = self.getMaterialObjects(analysisObject)
         if len(materialObjects) == 0:
@@ -46,10 +32,8 @@ class CmdExportMesh:
         # and check the faces in the solid)
         # This is wrong, but user can set two materials to the same solid/face
 
-        meshNodes = shapeMeshObject.Nodes
-        nodesByMaterial = {}
         trianglesByMaterial = {}
-        materialPropsDict = {}
+        materialProperties = {}
 
         FreeCAD.Console.PrintMessage("Getting elements with material\n")
         for materialObject in materialObjects:
@@ -57,56 +41,27 @@ class CmdExportMesh:
             if len(elementsWithMaterial) == 0:
                 FreeCAD.Console.PrintError(f"No elements found with material {materialObject.Label}\n")
                 return
-
-            FreeCAD.Console.PrintMessage("Getting nodes from elements\n")
-            nodesWithMaterial = self.getNodesFromElements(elementsWithMaterial, femMeshObject)
-            if len(nodesWithMaterial) == 0:
-                FreeCAD.Console.PrintError(f"No nodes found with material {materialObject.Label}\n")
-                return
-            
+ 
             trianglesWithMaterial = self.getTrianglesFromElements(elementsWithMaterial, femMeshObject)
             if len(trianglesWithMaterial) == 0:
                 FreeCAD.Console.PrintError(f"No triangles found with material {materialObject.Label}\n")
                 return
             
             # TODO: is better key to be the name, label, id or something else?
-            nodesByMaterial[materialObject.Name] = nodesWithMaterial
             trianglesByMaterial[materialObject.Name] = trianglesWithMaterial
-            materialPropsDict[materialObject.Name] = materialObject.Material
+            print(materialObject.Material)
+            materialProperties[materialObject.Name] = self.getMaterialProperties(materialObject.Material)
+            if not materialProperties[materialObject.Name]:
+                FreeCAD.Console.PrintError(f"Some of the material properties are missing in {materialObject.Label}\n")
+                FreeCAD.Console.PrintError("The expected properties are: ThermalConductivity, SpecificHeat, Density, InitialTemperature\n")
+                return
 
-        # writing path
+        # Writing path
         path = os.path.dirname(document.FileName)
-        FreeCAD.Console.PrintMessage(f"Writing to file {path}/mesh.txt\n")
+        
         # Open file and write
-        with open("mesh.txt", "w") as file:
-            file.write("--Material properties--\n")
-            for material in materialPropsDict:
-                file.write(f"{material}: {materialPropsDict[material]}\n")
-            
-            file.write("\n--Nodes--\n")
-            for node in meshNodes:
-                file.write(f"N{node}: {list(shapeMeshObject.getNodeById(node))}\n")
-
-            file.write("\n--Triangles--\n")
-            for triangle in triangles:
-                file.write(f"T{triangle}: {[f'N{n}' for n in triangles[triangle]]}\n")
-
-            file.write("\n--Triangles by nodes--\n")
-            for node in sorted(trianglesByNode):
-                file.write(f"N{node}: {[f'T{t}' for t in trianglesByNode[node]]}\n")
-
-            file.write("\n--Nodes by material--\n")
-            for material in nodesByMaterial:
-                file.write(f"{material}: {[f'N{n}' for n in nodesByMaterial[material]]}\n")
-
-            file.write("\n--Triangles by material--\n")
-            for material in nodesByMaterial:
-                file.write(f"{material}: {[f'T{t}' for t in trianglesByMaterial[material]]}\n")
-
-        FreeCAD.Console.PrintMessage("Exported mesh to mesh.txt\n")
-
-        FreeCAD.Console.PrintMessage(f"Writing to file {path}/mesh.vtk\n")
-        femMeshObject.FemMesh.write("mesh.vtk")
+        self.writeMaterialAsJson(materialProperties, trianglesByMaterial, path)
+        self.writeFemMeshAsVtk(femMeshObject, path)
 
     def IsActive(self):
         return bool(FreeCAD.activeDocument())
@@ -143,6 +98,30 @@ class CmdExportMesh:
                 materialObjects.append(object)
         return materialObjects
     
+    def getMaterialProperties(self, material):
+        """Returns a dictionary of the neccessary properties of the material"""
+        # TODO: move to constants file
+        MATERIAL_PROPERTIES = [
+            "ThermalConductivity",
+            "SpecificHeat",
+            "Density",
+            "InitialTemperature" # TODO: move initial temperature to another place
+        ]
+
+        # Check if every material property exists in the material
+        for property in MATERIAL_PROPERTIES:
+            if property not in material:
+                return None
+
+        # Format the material properties to remove units
+        materialWithoutUnits = {}
+        for property in MATERIAL_PROPERTIES:
+            matches = re.findall(r"\d+.\d+|\d+", material[property])
+            if matches:
+                materialWithoutUnits[property] = float(matches[0])
+
+        return materialWithoutUnits
+    
     def getElementsWithMaterial(self, materialObject):
         """Returns a list of elements (solid or faces) with the material"""
         references = materialObject.References
@@ -151,30 +130,13 @@ class CmdExportMesh:
         for reference in references:
             part, elementNames = reference
             for elementName in elementNames:
-                # TODO: is okay to assume getSubObject exists in part?
                 element = part.getSubObject(elementName)
                 elementsWithMaterial.append(element)
 
         return elementsWithMaterial
-    
-    def getNodesFromElements(self, elements, femMeshObject):
-        """Returns a list of nodes from a list of elements"""
-        nodes = []
-        shape = femMeshObject.FemMesh
-
-        for element in elements:
-            if element.ShapeType == "Solid":
-                nodes.extend(shape.getNodesBySolid(element))
-            elif element.ShapeType == "Face":
-                nodes.extend(shape.getNodesByFace(element))
-            else:
-                FreeCAD.Console.PrintError(f"Element {element.Name} is not a solid or face\n")
-                FreeCAD.Console.PrintError(f"This could be causing an error on the mesh generation\n")
-
-        return nodes
-    
+        
     def getTrianglesFromElements(self, elements, femMeshObject):
-        """Returns a list of triangles from a list of elements"""
+        """Returns a stringify list of triangles from a list of elements"""
         triangles = []
         shape = femMeshObject.FemMesh
 
@@ -191,4 +153,27 @@ class CmdExportMesh:
                 FreeCAD.Console.PrintError(f"Element {element.Name} is not a solid or face\n")
                 FreeCAD.Console.PrintError(f"This could be causing an error on the mesh generation\n")
 
-        return triangles
+        return str(triangles)
+    
+    def writeFemMeshAsVtk(self, femMeshObject, path):
+        """Writes the mesh as a vtk file"""
+        FreeCAD.Console.PrintMessage(f"Writing to file {path}/mesh.vtk\n")
+        femMeshObject.FemMesh.write("mesh.vtk")
+        FreeCAD.Console.PrintMessage(f"Exported mesh to file {path}/mesh.vtk\n")
+
+    def writeMaterialAsJson(self, materialProperties, trianglesByMaterial, path):
+        """Writes the material as a json file"""
+        FreeCAD.Console.PrintMessage(f"Writing to file {path}/mesh.json\n")
+
+        with open("mesh.json", "w") as file:
+            json.dump(
+                {
+                    "materialProps": materialProperties,
+                    "trianglesByMaterial": trianglesByMaterial
+                },
+                file,
+                indent=4
+            )
+
+        FreeCAD.Console.PrintMessage(f"Exported mesh to file {path}/mesh.json\n")
+

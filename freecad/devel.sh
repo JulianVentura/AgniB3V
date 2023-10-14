dfd8e074d706645fa83aa899a914c3c5a52e0587
Julian Ventura <jventura@fi.uba.ar>
2023-10-14 17:29:46 -0300

    Updated devel.sh and install.sh scripts to account for different FreeCAD installation directories

--- freecad/devel.sh
+++ freecad/devel.sh
@@ -1,7 +1,30 @@
 #!/bin/sh
-echo "Installing Thermal Analysis workbench development version..."
-sudo rm -rf "$HOME/.local/share/FreeCAD/Mod/ThermalB3V"
-sudo mkdir -p "$HOME/.local/share/FreeCAD/Mod"
-sudo ln -sf "$(pwd)/ThermalB3V" "$HOME/.local/share/FreeCAD/Mod/ThermalB3V"
-echo "Workbench installed"
+
+sudo echo "Installing Thermal Analysis workbench development version"
+
+echo "Checking FreeCAD installation directory"
+
+freecad_dir1="$HOME/.local/share/FreeCAD"
+freecad_dir2="$HOME/.var/app/org.freecadweb.FreeCAD/data/FreeCAD"
+
+if [ -d "$freecad_dir1" ]; then
+    freecad_directory="$freecad_dir1"
+elif [ -d "$freecad_dir2" ]; then
+    freecad_directory="$freecad_dir2"
+else
+    echo "FreeCAD is not installed in your system"
+    exit 1
+fi
+
+plugin_name="ThermalB3V"
+plugin_dir="${freecad_directory}/Mod/${plugin_name}"
+
+
+sudo rm -rf "${plugin_dir}"
+sudo mkdir -p "${freecad_directory}/Mod"
+sudo ln -sf "$(pwd)/${plugin_name}" "${plugin_dir}"
+
+echo "Symlink created in: ${plugin_dir}"
+
+echo "${plugin_name} plugin development version installed sucessfully"
 

f15d01e9dfd9dbfe9a36a22c0aff4c625cd4ea39
Franco <fbarreneche@fi.uba.ar>
2023-10-15 18:13:46 -0300

    Add args, test files and README

--- standalone-raytrace/main.py
+++ standalone-raytrace/main.py
@@ -1,26 +1,66 @@
-import trimesh
+import sys
+import json
 from src import properties_atlas, vtk_io, view_factors, visualization
 
-def main():
-	visualization.ENABLED = True
+def op_process_view_factors(argv):
+	if(argv[1] != 'process' or len(argv) != 6):
+		return False
 
-	mesh = vtk_io.load_vtk("./models/cube.vtk")
-	point_sun_view_factors = view_factors.point_sun(mesh, [1,0,0], 0.05)
-	print("Node-Sun view factors:", point_sun_view_factors)
+	calle_name, op, mesh_file_path, properties_file_path, output_path, sun_direction = sys.argv
+	
+	mesh = vtk_io.load_vtk(mesh_file_path)	
+	sun_direction = list(map(float, sun_direction.strip("[]").split(",")))
+	props = properties_atlas.PropertiesAtlas(len(mesh.triangles), properties_file_path)
 
-	props = properties_atlas.PropertiesAtlas(len(mesh.triangles), './models/cube_materials.json')
-	print(len(mesh.triangles))
-	visualization.view_material(mesh, props)
-
-	mesh = trimesh.load("./models/three_squares.vtk", force='mesh')
-	element_element_view_factors = view_factors.element_element(mesh, 100)
-	print("Element-Element view factors:\n", element_element_view_factors)
+	point_sun_view_factors = view_factors.point_sun(mesh, sun_direction, 0.05)
+	element_element_view_factors = view_factors.element_element(mesh, 10000)
     
 	props.add_prop("view_factors", {
 		"sun": list(point_sun_view_factors),
 		"elements": list(map(list, element_element_view_factors))
     })
-	props.dump("output.json")
+	props.dump(output_path)
+
+	return True
+
+def op_visualize_view_factors(argv):
+	if(argv[1] != 'viewvf' or len(argv) != 5):
+		return False
+	
+	calle_name, op, mesh_file_path, properties_file_path, element_id = sys.argv
+	mesh = vtk_io.load_vtk(mesh_file_path)
+	element_id = int(element_id)
+	material_file = open(properties_file_path)
+	material_json = json.load(material_file)
+	props = properties_atlas.PropertiesAtlas(len(mesh.triangles), properties_file_path)
+	visualization.view_view_factors(mesh, element_id, material_json["view_factors"]["elements"][element_id])
+
+	return True
+
+def op_visualize_material(argv):
+	if(argv[1] != 'viewm' or len(argv) != 4):
+		return False
+	calle_name, op, mesh_file_path, properties_file_path = sys.argv
+	
+	mesh = vtk_io.load_vtk(mesh_file_path)
+	props = properties_atlas.PropertiesAtlas(len(mesh.triangles), properties_file_path)
+	visualization.view_material(mesh, props)
+
+	return True
+
+def op_default(argv):
+	print("Use:")
+	print(f"{argv[0]} process mesh_file_path properties_file_path output_path sun_direction")
+	print(f"{argv[0]} viewvf mesh_file_path properties_file_path element_id")
+	print(f"{argv[0]} viewm mesh_file_path properties_file_path")
+
+	return True
+
+def main():
+	ops = [op_process_view_factors, op_visualize_view_factors, op_visualize_material, op_default]
+	for op in ops:
+		if(op(sys.argv)):
+			return
 
 if __name__ == '__main__':
 	main()

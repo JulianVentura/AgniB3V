665abfa07dc9d82a2d05c46ee27131ddffc123d2
guidobotta <gbotta@fi.uba.ar>
2023-10-18 23:48:17 -0300

    Refactor dialogs and export mesh

--- freecad/ThermalB3V/ui/DialogGlobalProperties.py
+++ freecad/ThermalB3V/ui/DialogGlobalProperties.py
@@ -1,5 +1,6 @@
-from PySide import QtGui, QtCore
-from PySide2.QtWidgets import QDialog, QVBoxLayout, QHBoxLayout, QGridLayout
+from PySide2.QtWidgets import *
+from PySide2.QtCore import *
+from ui.WidgetGlobalProperties import WidgetGlobalProperties
 
 class DialogGlobalProperties(QDialog):
     """
@@ -15,74 +16,17 @@ class DialogGlobalProperties(QDialog):
     def initUI(self):
         # create our window
         self.setWindowTitle("Global Properties")
-        self.setWindowFlags(QtCore.Qt.WindowStaysOnTopHint)
+        self.setWindowFlags(Qt.WindowStaysOnTopHint)
+        self.horizontalLayout = QHBoxLayout(self)
+        self.frame = QFrame(self)
+        self.horizontalLayout_2 = QHBoxLayout(self.frame)
 
-        globalProperties = self.workbench.getGlobalPropertiesInfo()
+        self.globalProperties = WidgetGlobalProperties(self.frame, self.workbench, self.onClose)
 
-        # create inputs
-        for property in globalProperties:
-            self.create_input(
-                property,
-                globalProperties[property]['label'],
-                globalProperties[property]['unit'],
-                str(globalProperties[property]['value']),
-            )
-
-        # create import button
-        importButton = QtGui.QPushButton('Import properties', self)
-        importButton.setDefault(False)
-        importButton.setAutoDefault(False)
-        importButton.clicked.connect(self.onImportProperties)
-        
-        # create OK button
-        okButton = QtGui.QPushButton('OK', self)
-        okButton.setDefault(False)
-        okButton.setAutoDefault(False)
-        okButton.clicked.connect(self.onOk)
-
-        layout = QVBoxLayout()
-
-        line = QHBoxLayout()
-        layout.addLayout(line)
-
-        line = QGridLayout()
-
-        # Add inputs to layout
-        row = 0
-
-        for property in globalProperties:
-            line.addWidget(getattr(self, property + "Label"), row, 0)
-            line.addWidget(getattr(self, property + "Input"), row, 1)
-            row += 1
-
-        layout.addLayout(line)
-
-        line = QHBoxLayout()
-        line.addStretch()
-        line.addWidget(okButton)
-        line.addWidget(importButton)
-        layout.addLayout(line)
-        self.setLayout(layout)
-
+        self.horizontalLayout_2.addWidget(self.globalProperties)
+        self.horizontalLayout.addWidget(self.frame)    
         self.show()
 
-    def onImportProperties(self):
-        """Imports the global properties from the selected json file"""
-        file_path, _ = QtGui.QFileDialog.getOpenFileName(None, "Select a file to import", "", "JSON Files (*.json)")
-        if file_path:
-            self.workbench.importProperties(file_path)
-            # update inputs
-            globalProperties = self.workbench.getGlobalPropertiesInfo()
-            for property in globalProperties:
-                getattr(self, property + "Input").setText(str(globalProperties[property]['value']))
-
-    def create_input(self, attribute_name, label, unit, value):
-        qtLabel = QtGui.QLabel(label + (f" ({unit})" if unit else ""), self)
-        qtInput = QtGui.QLabel(value, self)
-
-        setattr(self, attribute_name + "Label", qtLabel)
-        setattr(self, attribute_name + "Input", qtInput)
-
-    def onOk(self):
+    def onClose(self):
         """Closes the dialog"""
         self.close()

dc227ea58326bff41e16ecca3a34f5fdaf6ffd47
Julian Ventura <jventura@fi.uba.ar>
2023-10-23 20:25:16 -0300

    Add dependencies to raycasting script

--- standalone-raytrace/test.sh
+++ standalone-raytrace/test.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 python3 main.py process ./models/three_squares.vtk ./models/three_squares_materials.json ./three_squares_properties.json [1,0,0] true
 python3 main.py viewm ./models/three_squares.vtk ./models/three_squares_materials.json
 python3 main.py viewvf ./models/three_squares.vtk ./three_squares_properties.json 2

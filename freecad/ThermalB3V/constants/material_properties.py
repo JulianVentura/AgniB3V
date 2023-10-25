7a661275ca10e51d35a2b98c471534f52fc14151
guidobotta <gbotta@fi.uba.ar>
2023-10-24 23:11:38 -0300

    Add missing global and material properties

--- freecad/ThermalB3V/constants/material_properties.py
+++ freecad/ThermalB3V/constants/material_properties.py
@@ -2,5 +2,10 @@ MATERIAL_PROPERTIES = [
     "ThermalConductivity",
     "SpecificHeat",
     "Density",
+    "Flux",
+    "Absorptanse",
+    "Thickness",
+    "AlphaSun",
+    "AlphaIR",
     "InitialTemperature", # TODO: move initial temperature to another place
 ]

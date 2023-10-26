f481871486d9ab7fa0a644897c997eb976d09d44
guidobotta <gbotta@fi.uba.ar>
2023-10-25 23:07:03 -0300

    feat: add workbench setting path on utils

--- freecad/ThermalB3V/public/utils.py
+++ freecad/ThermalB3V/public/utils.py
@@ -5,9 +5,24 @@ public_path = path.dirname(path.realpath(__file__))
 icons_path = path.join(public_path, 'icons')
 
 def workbenchPath():
+    """
+    It returns the workbench path
+    """
     return workbench_path
 
 def iconPath(name):
-    f = path.join(icons_path, name)
+    """
+    It returns the icon path given the icon name
+    """
+    iconPath = path.join(icons_path, name)
 
-    return f
+    return iconPath
+
+def getWorkbenchSettingsPath(documentPath):
+    """
+    It returns the workbench settings path given the document path
+    """
+    # TODO: maybe is better to return a directory instead of a file
+    workbenchSettingsPath = path.join(documentPath, 'workbenchSettings.json')
+
+    return workbenchSettingsPath

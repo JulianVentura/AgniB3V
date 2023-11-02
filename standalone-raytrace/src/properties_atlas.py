26b7106f17002b6895c72d7537c5920c3941aa54
guidobotta <gbotta@fi.uba.ar>
2023-11-02 20:29:49 -0300

    docs: add properties atlas  documentation

--- standalone-raytrace/src/properties_atlas.py
+++ standalone-raytrace/src/properties_atlas.py
@@ -2,7 +2,16 @@ import numpy as np
 import json
 
 class PropertiesAtlas():
+    """
+    Implements a class that loads the properties of the materials from a json file.
+    """
+
     def __init__(self, elements_amount, material_file_path):
+        """
+        Receives the amount of elements and the path to the json file.
+        It loads the json file and creates a list of materials and a list of
+        materials by element.
+        """
         self.material_by_element = np.full(elements_amount,-1)
         self.materials = []
 
@@ -22,11 +31,21 @@ class PropertiesAtlas():
                 print(f"Warning: Element {element_id} does not have a material")
 
     def get_material_props(self, element_index):
+        """
+        Recieven an index and returns the properties of the material of the element
+        with the given index.
+        """
         return self.materials[self.material_by_element[element_index]]
     
     def add_prop(self, key, data):
-        self.material_json[key] = data;
+        """
+        Receives a key and data and adds it to the material json.
+        """
+        self.material_json[key] = data
 
     def dump(self, output_path):
-	    with open(output_path, 'w', encoding='utf-8') as f:
+        """
+        Recieves an output path and dumps the material json to a file.
+        """
+        with open(output_path, 'w', encoding='utf-8') as f:
 	        json.dump(self.material_json, f, ensure_ascii=True)

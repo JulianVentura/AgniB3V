7443f4131908a3bc8c8d6de7dca0313b3eac46cb
guidobotta <gbotta@fi.uba.ar>
2023-11-03 19:43:40 -0300

    refact: properties atlas

--- standalone-raytrace/src/properties_atlas.py
+++ standalone-raytrace/src/properties_atlas.py
@@ -12,14 +12,15 @@ class PropertiesAtlas():
         It loads the json file and creates a list of materials and a list of
         materials by element.
         """
+        print(f"Loading material file from {material_file_path}")
+        with open(material_file_path) as material_file:
+            self.properties_json = json.load(material_file)
+
         self.material_by_element = np.full(elements_amount,-1)
         self.materials = []
 
-        material_file = open(material_file_path)
-        self.material_json = json.load(material_file)
-
-        material_json_props = self.material_json["materials"]["properties"]
-        material_json_elements = self.material_json["materials"]["elements"]
+        material_json_props = self.properties_json["materials"]["properties"]
+        material_json_elements = self.properties_json["materials"]["elements"]
         for material_name, material_elements in material_json_elements.items():
             material_idx = len(self.materials)
             self.materials.append(material_json_props[material_name])
@@ -41,11 +42,17 @@ class PropertiesAtlas():
         """
         Receives a key and data and adds it to the material json.
         """
-        self.material_json[key] = data
+        self.properties_json[key] = data
+
+    def get_global_prop(self, key):
+        """
+        Recives a global property key and returns the data associated with it.
+        """
+        return self.properties_json["global_properties"][key]
 
     def dump(self, output_path):
         """
         Recieves an output path and dumps the material json to a file.
         """
         with open(output_path, 'w', encoding='utf-8') as f:
-	        json.dump(self.material_json, f, ensure_ascii=True)
+	        json.dump(self.properties_json, f, ensure_ascii=True)

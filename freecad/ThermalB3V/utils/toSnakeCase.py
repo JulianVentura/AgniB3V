432ad738b415802faf8a3fe43f96822be6817d1c
guidobotta <gbotta@fi.uba.ar>
2023-11-07 19:06:39 -0300

    feat: add string utils

--- freecad/ThermalB3V/utils/toSnakeCase.py
+++ freecad/ThermalB3V/utils/toSnakeCase.py
@@ -1,5 +1,9 @@
 import re
 
 def toSnakeCase(s):
+    """
+    Receives a string in the format "camelCase"
+    and returns "camel_case"
+    """
     s1 = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', s)
     return re.sub('([a-z0-9])([A-Z])', r'\1_\2', s1).lower()

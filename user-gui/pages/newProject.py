ddc62b0dd4bfb857e271ba7fc7139736f57714db
guidobotta <gbotta@fi.uba.ar>
2023-11-28 20:09:16 -0300

    refact(gui): pages

--- user-gui/pages/newProject.py
+++ user-gui/pages/newProject.py
@@ -2,6 +2,7 @@ from PySide2.QtCore import *
 from PySide2.QtGui import *
 from PySide2.QtWidgets import *
 import os
+from utils.appState import AppState
 
 class NewProjectWidget(QWidget):
     def __init__(self, parent=None):
@@ -10,76 +11,96 @@ class NewProjectWidget(QWidget):
         self.setupUi()
 
     def setupUi(self):
-        self.mainLayout = QVBoxLayout(self)
-        self.frame = QFrame(self)
-        self.frame.setFrameShape(QFrame.StyledPanel)
-        self.frame.setFrameShadow(QFrame.Raised)
-        self.verticalLayout_5 = QVBoxLayout(self.frame)
-        self.horizontalLayout_4 = QHBoxLayout()
-        self.horizontalSpacer_3 = QSpacerItem(40, 20, QSizePolicy.Expanding, QSizePolicy.Minimum)
+        mainLayout = QVBoxLayout(self)
+        frame = QFrame(self)
+        frame.setFrameShape(QFrame.StyledPanel)
+        frame.setFrameShadow(QFrame.Raised)
+        verticalLayout = QVBoxLayout(frame)
+        goBackButton = QPushButton(frame)
+        goBackButton.setText(QCoreApplication.translate("Dialog", u"<", None))
+        goBackButton.setFixedSize(30, 30)
+        goBackButton.clicked.connect(self.goBack)
 
-        self.label = QLabel(self.frame)
+        headerLayout = self.getHeaderLayout(frame)
+        bodyLayout = self.getBodyLayout(frame)
+
+        verticalLayout.addLayout(headerLayout)
+        verticalLayout.addLayout(bodyLayout)
+        mainLayout.addWidget(frame)
+
+    def getHeaderLayout(self, frame):
+        """
+        Returns the header layout.
+        """
+        headerLayout = QHBoxLayout()
+
+        horizontalSpacerLeft = QSpacerItem(40, 20, QSizePolicy.Expanding, QSizePolicy.Minimum)
+
+        title = QLabel(frame)
         font = QFont()
         font.setPointSize(24)
-        self.label.setFont(font)
-        self.label.setText(QCoreApplication.translate("Dialog", u"Thermal B3V", None))
+        title.setFont(font)
+        title.setText(QCoreApplication.translate("Dialog", u"Thermal B3V", None))
 
-        self.horizontalSpacer_4 = QSpacerItem(40, 20, QSizePolicy.Expanding, QSizePolicy.Minimum)
+        horizontalSpacerRight = QSpacerItem(40, 20, QSizePolicy.Expanding, QSizePolicy.Minimum)
 
-        self.verticalSpacer_2 = QSpacerItem(20, 40, QSizePolicy.Minimum, QSizePolicy.Expanding)
+        headerLayout.addItem(horizontalSpacerLeft)
+        headerLayout.addWidget(title)
+        headerLayout.addItem(horizontalSpacerRight)
+        return headerLayout
+    
+    def getBodyLayout(self, frame):
+        verticalLayout = QVBoxLayout()
+        verticalSpacer = QSpacerItem(20, 40, QSizePolicy.Minimum, QSizePolicy.Expanding)
 
-        self.horizontalLayout = QHBoxLayout()
-        self.verticalLayout = QVBoxLayout()
-        self.label_2 = QLabel(self.frame)
-        self.label_2.setText(QCoreApplication.translate("Dialog", u"Directorio", None))
+        horizontalLayout = QHBoxLayout()
+        labelLayout = QVBoxLayout()
+        directoryLabel = QLabel(frame)
+        directoryLabel.setText(QCoreApplication.translate("Dialog", u"Directorio", None))
 
-        self.label_3 = QLabel(self.frame)
-        self.label_3.setText(QCoreApplication.translate("Dialog", u"Nombre del Proyecto", None))
+        projectNameLabel = QLabel(frame)
+        projectNameLabel.setText(QCoreApplication.translate("Dialog", u"Nombre del Proyecto", None))
 
-        self.verticalLayout_3 = QVBoxLayout()
-        self.lineEdit = QLineEdit(self.frame)
-        self.lineEdit_2 = QLineEdit(self.frame)
+        lineEditLayout = QVBoxLayout()
+        directoryEdit = QLineEdit(frame)
+        projectNameEdit = QLineEdit(frame)
 
-        self.frame1 = QFrame(self.frame)
-        self.verticalLayout_4 = QVBoxLayout(self.frame1)
-        self.verticalLayout_4.setContentsMargins(0, 0, 0, 0)
-        self.toolButton = QToolButton(self.frame1)
-        self.toolButton.setAutoFillBackground(False)
-        self.toolButton.setText(QCoreApplication.translate("Dialog", u"...", None))
-        self.toolButton.clicked.connect(self.onSelectDirectory)
+        dirButtonFrame = QFrame(frame)
+        dirButtonLayout = QVBoxLayout(dirButtonFrame)
+        dirButtonLayout.setContentsMargins(0, 0, 0, 0)
+        directoryButton = QToolButton(dirButtonFrame)
+        directoryButton.setAutoFillBackground(False)
+        directoryButton.setText(QCoreApplication.translate("Dialog", u"...", None))
+        directoryButton.clicked.connect(self.onSelectDirectory)
 
-        self.verticalSpacer = QSpacerItem(20, 146, QSizePolicy.Minimum, QSizePolicy.Expanding)
+        verticalSpacer_2 = QSpacerItem(20, 146, QSizePolicy.Minimum, QSizePolicy.Expanding)
 
-        self.horizontalLayout_3 = QHBoxLayout()
-        self.horizontalSpacer_2 = QSpacerItem(40, 20, QSizePolicy.Expanding, QSizePolicy.Minimum)
+        createButtonLayout = QHBoxLayout()
+        horizontalSpacer = QSpacerItem(40, 20, QSizePolicy.Expanding, QSizePolicy.Minimum)
 
-        self.pushButton = QPushButton(self.frame)
-        self.pushButton.clicked.connect(self.onNewProject)
-        self.pushButton.setText(QCoreApplication.translate("Dialog", u"Crear Proyecto", None))
+        createButton = QPushButton(frame)
+        createButton.clicked.connect(self.onNewProject)
+        createButton.setText(QCoreApplication.translate("Dialog", u"Crear Proyecto", None))
         
-        self.horizontalSpacer_5 = QSpacerItem(40, 20, QSizePolicy.Expanding, QSizePolicy.Minimum)
+        horizontalSpacer_2 = QSpacerItem(40, 20, QSizePolicy.Expanding, QSizePolicy.Minimum)
 
-        self.horizontalLayout_4.addItem(self.horizontalSpacer_3)
-        self.horizontalLayout_4.addWidget(self.label)
-        self.horizontalLayout_4.addItem(self.horizontalSpacer_4)
-        self.verticalLayout_5.addLayout(self.horizontalLayout_4)
-        self.verticalLayout_5.addItem(self.verticalSpacer_2)
-        self.verticalLayout.addWidget(self.label_2)
-        self.verticalLayout.addWidget(self.label_3)
-        self.horizontalLayout.addLayout(self.verticalLayout)
-        self.verticalLayout_3.addWidget(self.lineEdit)
-        self.verticalLayout_3.addWidget(self.lineEdit_2)
-        self.horizontalLayout.addLayout(self.verticalLayout_3)
-        self.verticalLayout_4.addWidget(self.toolButton)
-        self.horizontalLayout.addWidget(self.frame1, 0, Qt.AlignTop)
-        self.verticalLayout_5.addLayout(self.horizontalLayout)
-        self.verticalLayout_5.addItem(self.verticalSpacer)
-        self.horizontalLayout_3.addItem(self.horizontalSpacer_2)
-        self.horizontalLayout_3.addWidget(self.pushButton)
-        self.horizontalLayout_3.addItem(self.horizontalSpacer_5)
-        self.verticalLayout_5.addLayout(self.horizontalLayout_3)
+        labelLayout.addWidget(directoryLabel)
+        labelLayout.addWidget(projectNameLabel)
+        horizontalLayout.addLayout(labelLayout)
+        lineEditLayout.addWidget(directoryEdit)
+        lineEditLayout.addWidget(projectNameEdit)
+        horizontalLayout.addLayout(lineEditLayout)
+        dirButtonLayout.addWidget(directoryButton)
+        horizontalLayout.addWidget(dirButtonFrame, 0, Qt.AlignTop)
+        createButtonLayout.addItem(horizontalSpacer)
+        createButtonLayout.addWidget(createButton)
+        createButtonLayout.addItem(horizontalSpacer_2)
+        verticalLayout.addItem(verticalSpacer)
+        verticalLayout.addLayout(horizontalLayout)
+        verticalLayout.addItem(verticalSpacer_2)
+        verticalLayout.addLayout(createButtonLayout)
 
-        self.mainLayout.addWidget(self.frame)
+        return verticalLayout
 
     def onSelectDirectory(self):
         """
@@ -112,6 +133,7 @@ class NewProjectWidget(QWidget):
         
         # TODO: copiar templates
         
+        AppState().set("projectDirectory", directory)
         self.parent.setCurrentIndex(2)
 
     def _validateDirectoryAndName(self, directory, projectName) -> bool:
@@ -137,3 +159,9 @@ class NewProjectWidget(QWidget):
                 return False
         
         return True
+    
+    def goBack(self):
+        """
+        Returns to the landing page.
+        """
+        self.parent.setCurrentIndex(0)

1a3196f18a576812256c0e4c6e3874e524a37b7e
guidobotta <gbotta@fi.uba.ar>
2023-12-01 16:44:32 -0300

    feat: add global config and routing

--- user-gui/utils/appState.py
+++ user-gui/utils/appState.py
@@ -1,3 +1,5 @@
+from .globalConfiguration import GlobalConfiguration
+
 class AppStateMeta(type):
     _instance = None
 
@@ -9,7 +11,27 @@ class AppStateMeta(type):
 
 class AppState(metaclass=AppStateMeta):
     def __init__(self):
+        self.globalConfiguration = GlobalConfiguration()
         self.projectDirectory = None
+        self.routes = []
 
     def openProject(self, directory):
         self.projectDirectory = directory
+
+    def addRoute(self, route):
+        self.routes.append(route)
+
+    def resetRoutes(self):
+        self.routes = []
+    
+    def popLastRoute(self) -> int:
+        print(self.routes)
+        if len(self.routes) == 0:
+            return 0
+        return self.routes.pop()
+
+    def getGlobalConfiguration(self) -> dict:
+        return self.globalConfiguration.config
+    
+    def setGlobalConfiguration(self, config):
+        self.globalConfiguration.updateConfig(config)

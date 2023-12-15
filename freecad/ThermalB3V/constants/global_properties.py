35854d1595a918f1624b02152e0e35c272f06038
guidobotta <gbotta@fi.uba.ar>
2023-12-14 21:27:55 -0300

    refact: remove initial temperature from global props

--- freecad/ThermalB3V/constants/global_properties.py
+++ freecad/ThermalB3V/constants/global_properties.py
@@ -20,11 +20,6 @@ GLOBAL_PROPERTIES_INPUTS = {
         "unit": "K",
         "value": 4.0,
     },
-    "initialTemperature": {
-        "label": "Initial temperature",
-        "unit": "K",
-        "value": 200.0,
-    },
     "internalEmission": {
         "label": "Internal emission",
         "unit": "",

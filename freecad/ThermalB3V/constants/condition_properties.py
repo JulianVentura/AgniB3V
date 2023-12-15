7a1f9b6c54d3cb56cc0a660aba2bd431628dc0f8
guidobotta <gbotta@fi.uba.ar>
2023-12-15 00:39:47 -0300

    refact(freecad): condition list to dict

--- freecad/ThermalB3V/constants/condition_properties.py
+++ freecad/ThermalB3V/constants/condition_properties.py
@@ -1,5 +1,13 @@
-# List of condition properties. They must be camelCase
-CONDITION_PROPERTIES = [
-    "flux",
-    "initialTemperature",
-]
+# Dict of condition properties
+CONDITION_PROPERTIES = {
+    "flux": {
+        "label": "Flux",
+        "unit": "W/m2",
+        "value": 0.0,
+    },
+    "initialTemperature": {
+        "label": "Initial temperature",
+        "unit": "K",
+        "value": 200.0,
+    },
+}

a78ae3d255defedb14433757c3294f75aa42f4c6
Franco <fbarreneche@fi.uba.ar>
2023-12-17 17:04:09 -0300

    Refactor visualization and clean outdated functions

--- standalone-raytrace/src/view_factors.py
+++ standalone-raytrace/src/view_factors.py
@@ -31,9 +31,9 @@ def element_earth(
     ir_view_factors = np.zeros(utils.element_amount(mesh.triangles))
     albedo_view_factors = np.zeros(utils.element_amount(mesh.triangles))
 
-    for element_idx in range(utils.element_amount(mesh.triangles)):
-        emitting_element = mesh.triangles[element_idx]
-        emitting_element_normal = element_normals[element_idx]
+    for element_id in range(utils.element_amount(mesh.triangles)):
+        emitting_element = mesh.triangles[element_id]
+        emitting_element_normal = element_normals[element_id]
 
         ray_origins = utils.generate_random_points_in_element(
             emitting_element, ray_amount
@@ -43,7 +43,8 @@ def element_earth(
         ray_origins += ray_directions * RAY_DISPLACEMENT
 
         if DEBUG_VISUALIZATION_ENABLED:
-            visualization.view_raycast(mesh, element_idx, ray_origins, ray_directions)
+            visualization.view_raycast(ray_origins, ray_directions, mesh, element_id)
+
         hit_element_ids = mesh.ray.intersects_first(ray_origins, ray_directions)
         mask = np.ones(ray_amount, dtype=bool)
         mask[hit_element_ids >= 0] = 0
@@ -53,8 +54,8 @@ def element_earth(
         )
 
         if not_hit_ray_directions.size == 0:
-            ir_view_factors[element_idx] = 0
-            albedo_view_factors[element_idx] = 0
+            ir_view_factors[element_id] = 0
+            albedo_view_factors[element_id] = 0
             continue
 
         ray_sat_dot_product = np.abs(
@@ -65,7 +66,8 @@ def element_earth(
         ray_earth_dot_product = utils.array_dot(not_hit_ray_directions, earth_direction)
         ray_earth_dot_product[ray_earth_dot_product < 0] = 0
         ir_view_factor = IR_SCALE_FACTOR * ray_earth_dot_product * ray_sat_dot_product
-        ir_view_factors[element_idx] = np.sum(ir_view_factor) / ray_amount
+        ir_view_factors[element_id] = np.sum(ir_view_factor) / ray_amount
+
         # Albedo
         ray_sun_dot_product = utils.array_dot(not_hit_ray_directions, -sun_direction)
         albedo_view_factor = (
@@ -73,7 +75,7 @@ def element_earth(
             * ray_sat_dot_product
             * albedo_edge(ray_sun_dot_product, penumbra_fraction=penumbra_fraction)
         )
-        albedo_view_factors[element_idx] = np.sum(albedo_view_factor) / ray_amount
+        albedo_view_factors[element_id] = np.sum(albedo_view_factor) / ray_amount
 
     return ir_view_factors, albedo_view_factors
 
@@ -96,12 +98,6 @@ def element_sun(mesh, sun_direction):
     )
     intersected = mesh.ray.intersects_any(ray_origins, ray_directions)
     element_sun_view_factors[intersected] = 0
-
-    if DEBUG_VISUALIZATION_ENABLED:
-        invisible_nodes = np.arange(len(element_centers))[intersected]
-        visualization.view_invisible_points(
-            mesh, sun_direction, element_centers[invisible_nodes]
-        )
 
     return element_sun_view_factors
 
@@ -137,9 +133,9 @@ def element_element(
     element_normals = trimesh.triangles.normals(mesh.triangles)[0]
     view_factors = np.zeros((len(mesh.triangles), len(mesh.triangles)))
 
-    for element_idx in range(len(mesh.triangles)):
-        emitting_element = mesh.triangles[element_idx]
-        emitting_element_normal = element_normals[element_idx]
+    for element_id in range(len(mesh.triangles)):
+        emitting_element = mesh.triangles[element_id]
+        emitting_element_normal = element_normals[element_id]
         view_factors_row = np.zeros(len(mesh.triangles))
 
         # Original emission
@@ -153,7 +149,7 @@ def element_element(
         ray_origins += ray_directions * RAY_DISPLACEMENT
 
         if DEBUG_VISUALIZATION_ENABLED:
-            visualization.view_raycast(mesh, element_idx, ray_origins, ray_directions)
+            visualization.view_raycast(ray_origins, ray_directions, mesh, element_id)
 
         hit_element_ids, hit_ray_ids, hit_points = mesh.ray.intersects_id(
             ray_origins, ray_directions, return_locations=True, multiple_hits=False
@@ -183,7 +179,7 @@ def element_element(
 
             if DEBUG_VISUALIZATION_ENABLED:
                 visualization.view_raycast(
-                    mesh, element_idx, ray_origins, ray_directions
+                    ray_origins, ray_directions, mesh, element_id
                 )
 
             hit_element_ids, hit_ray_ids, hit_points = mesh.ray.intersects_id(
@@ -202,7 +198,7 @@ def element_element(
                 view_factors_row[absorbed_element_id] += 1
 
         view_factors_row /= ray_amount
-        view_factors[element_idx] = view_factors_row
+        view_factors[element_id] = view_factors_row
 
     return view_factors
 

89d58afd86dc505b41bafc385a6bbd54cea82fe1
Franco <fbarreneche@fi.uba.ar>
2023-12-17 15:54:39 -0300

    Add normal orientation visualization

--- standalone-raytrace/src/visualization.py
+++ standalone-raytrace/src/visualization.py
@@ -4,6 +4,7 @@ import seaborn as sb
 from . import utils
 
 RESET = "\033[0m"
+COLOR_BACKGROUND = [9, 10, 20]
 
 
 def _get_color_escape(rgb_color):
@@ -12,24 +13,47 @@ def _get_color_escape(rgb_color):
 
 
 def _color_item_str(rbg_color):
-    return _get_color_escape(rbg_color) + "███" + RESET
+    return _get_color_escape(rbg_color) + "██" + RESET
 
 
 def view_material(mesh, props):
-    colors = []
     print("REFERENCE:")
     palette = sb.color_palette("tab10", len(props.materials))
     for i in range(len(palette)):
         print(_color_item_str(palette[i]), props.materials[i]["name"])
-    mesh.unmerge_vertices()
+
+    colors = []
     for element_id in range(utils.element_amount(mesh.triangles)):
         material_id = props.material_by_element[element_id]
         colors.append(palette[material_id])
-
+    colors = np.array(colors)
+    colors = np.vstack((colors, colors))
+    mesh.faces = np.vstack((mesh.faces, np.fliplr(mesh.faces)))
+    mesh.unmerge_vertices()
     mesh.visual.vertex_colors = None
     mesh.visual.face_colors = colors
     scene = trimesh.Scene([mesh])
-    scene.show()
+    scene.show(background=COLOR_BACKGROUND, smooth=True)
+
+
+def view_normal(mesh):
+    print("REFERENCE:")
+    palette = np.array([[63, 96, 181], [165, 48, 48]]) / 255
+    print(_color_item_str(palette[0]), "+n")
+    print(_color_item_str(palette[1]), "-n")
+
+    mesh.faces = np.vstack((mesh.faces, np.fliplr(mesh.faces)))
+
+    side_amount = utils.element_amount(mesh.triangles) // 2
+    postiive_side_colors = np.broadcast_to(palette[0], (side_amount, 3))
+    negative_side_colors = np.broadcast_to(palette[1], (side_amount, 3))
+    side_colors = np.vstack((postiive_side_colors, negative_side_colors))
+
+    mesh.unmerge_vertices()
+    mesh.visual.vertex_colors = None
+    mesh.visual.face_colors = side_colors
+    scene = trimesh.Scene([mesh])
+    scene.show(background=COLOR_BACKGROUND, smooth=True)
 
 
 def view_raycast(mesh, emmiting_element_idx, ray_origins, ray_directions):

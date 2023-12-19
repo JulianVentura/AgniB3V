a5bfb7171c684c900bbdf41bdf7dd15f3870f045
Gianbelinche <gbelinche@fi.uba.ar>
2023-12-18 23:14:53 -0300

    Add time convergence

---
+++ graphics_template/convergence.py
@@ -0,0 +1,67 @@
+import vtk_parser
+import matplotlib.pyplot as plt
+import templates
+import numpy as np
+import std_deviation
+import cuadratic_error
+
+
+def plot_convergence(source, files, points, titles, label_generator, type, unit):
+    templates.template_style()
+
+    temperatures = []
+    for file in files:
+        temperatures.append(
+            vtk_parser.parse_results_vtk_series(
+                f"./sources/{source}/{file}/results",
+                "result.vtk.series",
+                lambda x: print(f"{x:.2f}"),
+            )[0]
+        )
+
+    graphs = []
+    names = []
+    for i in range(len(points)):
+        point = points[i]
+        fig, ax = plt.subplots()
+        temperatures_by_id = []
+        for j in range(len(files)):
+            times = []
+            temperatures_id = []
+            for time, result in temperatures[j].items():
+                temperatures_id.append(result[point[j]])
+                times.append(time)
+            ax.plot(times, temperatures_id, label=label_generator(files[j]))
+            temperatures_by_id.append(temperatures_id)
+        templates.template_plot(
+            ax,
+            "Time (s)",
+            "Temperature (K)",
+            f"{type} Convergence at {titles[i]}",
+        )
+        graphs.append(ax)
+        names.append(f"{source}_{titles[i]}")
+
+        ax = std_deviation.plot_std_deviation(times, temperatures_by_id)
+        templates.template_plot(
+            ax,
+            "Time (s)",
+            "Standard Deviation of Temperature (K)",
+            f"Standard Deviation over {type} Convergence at {titles[i]}",
+        )
+        graphs.append(ax)
+        names.append(f"{source}_std_deviation_{titles[i]}")
+
+        ax = cuadratic_error.plot_cuadratic_error(
+            times, temperatures_by_id, labels=[f + " " + unit for f in files]
+        )
+        templates.template_plot(
+            ax,
+            "Time (s)",
+            "Cuadratic Error of Temperature",
+            f"Cuadratic Error over {type} Convergence at {titles[i]} vs {files[-1]} {unit}",
+        )
+        graphs.append(ax)
+        names.append(f"{source}_cuadratic_error_{titles[i]}")
+
+    templates.template_save_multiple_images(graphs, source, names)
--- graphics_template/image_generator.py
+++ graphics_template/image_generator.py
@@ -1,10 +1,12 @@
 import energy
 import mesh_convergence
+import time_convergence
 
 
 def generate_images():
-    energy.plot()
+    # energy.plot()
     mesh_convergence.plot()
+    # time_convergence.plot()
 
 
 if __name__ == "__main__":

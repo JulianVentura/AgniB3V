4b23605e2edb8d640412973e93361a1c3cfe144a
Franco <fbarreneche@fi.uba.ar>
2023-12-20 16:58:23 -0300

    Document vector_math

--- standalone-raytrace/src/vector_math.py
+++ standalone-raytrace/src/vector_math.py
@@ -2,11 +2,20 @@ import numpy as np
 
 
 def normalize(vector):
+    """
+    Receives a vector and returns another vector with the same
+    direction and lenght one.
+    """
     norm = np.sqrt(vector.dot(vector))
     return vector / norm
 
 
 def proportionalize(vector):
+    """
+    Receives a vector and returns another vector where each
+    component is proportional to the original vector one
+    and the sum of all components is one.
+    """
     p = sum(vector)
     return vector / p
 
@@ -36,19 +45,34 @@ def spherical_cordinates(vector):
 
 
 def random_unit_vectors(amount):
+    """
+    Generates an array of unit vectors with pseudorandom directions.
+    """
     random_vectors = np.random.normal(0, 1, (amount, 3))
     return random_vectors / np.linalg.norm(random_vectors, axis=1)[:, np.newaxis]
 
 
 def array_dot(vectors, other_vector):
+    """
+    Receives an array of vectors and returns an array of inner product
+    between them and other vector.
+    """
     return vectors @ (other_vector)[:, np.newaxis]
 
 
 def array_array_dot(vectors, other_vectors):
+    """
+    Receives two array of vectors and returns an array of the inner products
+    between each pair of elements.
+    """
     return np.einsum("...j,...j", vectors, other_vectors)
 
 
 def array_array_cos(vectors, other_vectors):
+    """
+    Receives two array of vectors and returns an array of the cosine
+    of the angle between each pair of elements.
+    """
     dot_products = array_array_dot(vectors, other_vectors)
     norm_products = np.linalg.norm(vectors, axis=1) * np.linalg.norm(
         other_vectors, axis=1
@@ -58,6 +82,10 @@ def array_array_cos(vectors, other_vectors):
 
 # TODO: If vector is ortogonal to direction it will be multiplied by zero
 def orient_towards_direction(vectors, direction):
+    """
+    Receives a vector and flips it inplace if the inner product with
+    the direction is negative.
+    """
     vectors *= np.sign(vectors @ direction[:, np.newaxis])
 
 

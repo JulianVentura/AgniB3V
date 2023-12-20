cb701a742a616d4098c8a23ccb3711ff4a25632d
Franco <fbarreneche@fi.uba.ar>
2023-12-20 16:58:45 -0300

    Fix ray reflection test

--- standalone-raytrace/test/test_rays.py
+++ standalone-raytrace/test/test_rays.py
@@ -12,18 +12,17 @@ def test_aparent_element_area_multiplier():
     expected_aparent_area_multipliers = [1, 1, np.cos(np.pi / 4), np.cos(1.301819131)]
     assert np.allclose(aparent_area_multipliers, expected_aparent_area_multipliers)
 
-
 def test_reflected_rays_towards_element_normals():
     ray_directions = np.array([[0, 0, -1], [0, 1, -1], [0, 0, -1]])
-    element_normals = np.array([[0, 0, 1], [0, 0, 1], [0, 1, 1]])
+    element_normals = np.array([[0, 0, 1], [0, 0, 1], [0, 1/np.sqrt(2), 1/np.sqrt(2)]])
     reflected_rays = rays.reflected_rays(ray_directions, element_normals)
-    expected_reflected_rays = np.array([[0, 0, 1], [0, 1, 1], [0, 2, 1]])
-    assert np.array_equal(reflected_rays, expected_reflected_rays)
+    expected_reflected_rays = np.array([[0, 0, 1], [0, 1, 1], [0, 1, 0]])
+    assert np.allclose(reflected_rays, expected_reflected_rays)
 
 
 def test_reflected_rays_against_element_normals():
     ray_directions = np.array([[0, 0, 1], [0, 1, 1], [0, 0, 1]])
-    element_normals = np.array([[0, 0, 1], [0, 0, 1], [0, 1, 1]])
+    element_normals = np.array([[0, 0, 1], [0, 0, 1], [0, 1/np.sqrt(2), 1/np.sqrt(2)]])
     reflected_rays = rays.reflected_rays(ray_directions, element_normals)
-    expected_reflected_rays = np.array([[0, 0, -1], [0, 1, -1], [0, -2, -1]])
-    assert np.array_equal(reflected_rays, expected_reflected_rays)
+    expected_reflected_rays = np.array([[0, 0, -1], [0, 1, -1], [0, -1, -0]])
+    assert np.allclose(reflected_rays, expected_reflected_rays)

209f4c173a2b117a2fb0c66390d9f7bc856a3504
Gianbelinche <gbelinche@fi.uba.ar>
2023-12-21 18:34:32 -0300

    Add cpu vs gpu

--- graphics_template/image_generator.py
+++ graphics_template/image_generator.py
@@ -2,13 +2,15 @@ import energy
 import mesh_convergence
 import time_convergence
 import conduction
+import timings
 
 
 def generate_images():
     # energy.plot()
     # mesh_convergence.plot()
     # time_convergence.plot()
-    conduction.plot()
+    # conduction.plot()
+    timings.plot()
 
 
 if __name__ == "__main__":

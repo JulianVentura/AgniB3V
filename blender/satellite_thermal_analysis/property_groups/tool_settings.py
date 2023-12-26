80fca11953392d0614fd1cc24f938881fdf77c51
guidobotta <gbotta@fi.uba.ar>
2023-12-26 17:39:14 -0300

    chore: remove webgl and blender

--- blender/satellite_thermal_analysis/property_groups/tool_settings.py
+++
@@ -1,6 +0,0 @@
-import bpy
-
-class ThermalToolSettings(bpy.types.PropertyGroup):
-    solar_direction_vector : bpy.props.FloatVectorProperty(name="Solar Direction Vector", default=(0.0,0.0,-1.0))
-    ray_cast_displacement : bpy.props.FloatProperty(name="Desplazamiento de Raycast", default=0.01, min=0.001, max=2)
-    mesh_file_path : bpy.props.StringProperty(name="", description="Path to Directory", default="", maxlen=2048, subtype='FILE_PATH')

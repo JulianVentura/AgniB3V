b4c904b2b5902e3fa2b773240d3ce0ab191078b7
Gianbelinche <gbelinche@fi.uba.ar>
2023-12-30 18:21:36 -0300

    Add plotter

---
+++ plotter/UI.py
@@ -0,0 +1,216 @@
+import sys
+from PySide2.QtWidgets import (
+    QApplication,
+    QWidget,
+    QVBoxLayout,
+    QLabel,
+    QPushButton,
+    QComboBox,
+    QFileDialog,
+    QInputDialog,
+    QMessageBox,
+)
+from PySide2.QtGui import QIcon
+from PySide2.QtCore import Qt
+from plotter import (
+    plot_all_temperatures,
+    parse_results_vtk_series,
+    plot_temperature_by_id,
+    plot_average_temperature,
+    plot_std_temperature,
+)
+import os
+
+
+class MainWindow(QWidget):
+    def __init__(self, initial_file_path=None):
+        super().__init__()
+
+        self.layout = QVBoxLayout()
+        self.layout.setContentsMargins(50, 50, 50, 50)
+
+        self.label = QLabel("Select a graph type:")
+        self.label.setAlignment(Qt.AlignCenter)
+        self.layout.addWidget(self.label)
+
+        self.combo_box = QComboBox()
+        self.combo_box.addItems(
+            [
+                "All Temperatures Over Time",
+                "Temperature Over Time",
+                "Average Temperature Over Time",
+                "Standard Deviation Over Time",
+            ]
+        )
+        self.layout.addWidget(self.combo_box)
+
+        self.file_button = QPushButton("Choose File")
+        self.layout.addWidget(self.file_button, alignment=Qt.AlignCenter)
+        self.file_button.clicked.connect(self.open_file_dialog)
+
+        reload_icon = QIcon.fromTheme("view-refresh")
+        self.reload_button = QPushButton(reload_icon, "Reload")
+        self.layout.addWidget(self.reload_button, alignment=Qt.AlignCenter)
+        self.reload_button.clicked.connect(self.reload)
+        self.reload_button.hide()
+
+        self.button = QPushButton("Show Graph")
+        self.layout.addWidget(self.button, alignment=Qt.AlignCenter)
+        self.button.clicked.connect(self.show_graph)
+        self.button.hide()
+
+        self.loader_label = QLabel("Loading...", self)
+        self.loader_label.setAlignment(Qt.AlignCenter)
+        self.layout.addWidget(self.loader_label)
+        self.loader_label.hide()
+
+        self.file_label = QLabel("Chosen File: ", self)
+        self.file_label.setAlignment(Qt.AlignCenter)
+        self.layout.addWidget(self.file_label)
+        self.file_label.hide()
+
+        self.setLayout(self.layout)
+        self.setWindowTitle("Graph Selector")
+
+        self.results = None
+        self.file = initial_file_path
+
+    def load_initial(self):
+        """
+        Loads the initial data and results based on the selected file.
+        If a file is selected, it retrieves the results and updates the UI accordingly.
+        """
+        if self.file:
+            self.get_results(self.file)
+            self.loader_label.hide()
+            self.reload_button.show()
+
+    def show_graph(self):
+        """
+        Displays a graph based on the selected graph type.
+
+        The graph type is determined by the current text of the combo box.
+        The available graph types are:
+        - "All Temperatures Over Time": Plots all temperatures over time.
+        - "Temperature Over Time": Plots the temperature over time for a specific node ID.
+        - "Average Temperature Over Time": Plots the average temperature over time.
+        - "Standard Deviation Over Time": Plots the standard deviation of temperatures over time.
+        """
+        graph_type = self.combo_box.currentText()
+
+        if graph_type == "All Temperatures Over Time":
+            plot_all_temperatures(self.results)
+        elif graph_type == "Temperature Over Time":
+            num, ok = QInputDialog.getInt(self, "ID Selector", "Enter node ID:")
+            if ok and num >= 0:
+                plot_temperature_by_id(num, self.results)
+        elif graph_type == "Average Temperature Over Time":
+            plot_average_temperature(self.results)
+        elif graph_type == "Standard Deviation Over Time":
+            plot_std_temperature(self.results)
+
+    def get_file_with_extension(self, extension: str, directory: str) -> str or None:
+        """
+        Given an extension and a directory, it looks for a file with that extension in the
+        given directory and returns its path.
+        """
+        files = [os.path.join(directory, f) for f in os.listdir(directory)]
+        for file in files:
+            if file.endswith(extension):
+                return file
+        return None
+
+    def open_file_dialog(self):
+        """
+        Opens a file dialog to choose a directory that has a .vtk.series file.
+        After selecting the directory, it calls the get_results method with the
+        .vtk.series found inside the selected directory.
+        """
+        self.loader_label.show()
+        directory = QFileDialog.getExistingDirectory(self, "Select Directory")
+        file_name = self.get_file_with_extension(".vtk.series", directory)
+        if not file_name:
+            QMessageBox.critical(
+                self,
+                "Error",
+                "The selected directory does not contain a .vtk.series file.",
+            )
+            self.loader_label.hide()
+            return
+
+        self.get_results(file_name)
+
+        self.loader_label.hide()
+        self.reload_button.show()
+
+    def progress(self, percentage):
+        """
+        Update the progress label with the given percentage.
+
+        Args:
+            percentage (int): The progress percentage.
+
+        Returns:
+            None
+        """
+        percentage = int(percentage)
+        self.loader_label.setText(f"Loading... {percentage} %")
+        self.loader_label.show()
+        QApplication.processEvents()
+
+    def reload(self):
+        """
+        Reloads the results from the specified file and updates the UI.
+        """
+        self.loader_label.show()
+        file_name = self.file
+        self.get_results(file_name)
+        self.loader_label.hide()
+
+    def get_results(self, file_name):
+        """
+        Retrieves and parses the results from the specified file.
+
+        Args:
+            file_name (str): The name of the file to retrieve the results from.
+
+        Raises:
+            Exception: If the file type is invalid.
+
+        Returns:
+            None
+        """
+        if file_name:
+            try:
+                extension = os.path.splitext(file_name)[1]
+                if extension == ".series":
+                    self.results = parse_results_vtk_series(
+                        os.path.dirname(file_name),
+                        os.path.basename(file_name),
+                        self.progress,
+                    )[0]
+                else:
+                    raise Exception("Invalid file type")
+                self.file = file_name
+                self.file_label.setText("Chosen File: " + file_name)
+                self.file_label.show()
+                self.button.show()
+            except Exception as e:
+                QMessageBox.critical(
+                    self,
+                    "Error",
+                    e.args[0],
+                )
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    app.setStyle("Fusion")  # Use Fusion style
+    initial_file_path = None
+    if len(sys.argv) > 1:
+        initial_file_path = sys.argv[1]
+    window = MainWindow(initial_file_path)
+    window.setGeometry(100, 100, 400, 200)  # Set a fixed size for the window
+    window.show()
+    window.load_initial()
+    sys.exit(app.exec_())
---
+++ plotter/plotter.py
@@ -0,0 +1,296 @@
+import json
+import meshio
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib.widgets import Slider
+import sys
+
+# We have to install pip and then meshio for it to work
+
+
+def parse_vtk(vtk_path):
+    """
+    The function `parse_vtk` reads a VTK file and returns a dictionary of temperatures associated with
+    each point in the mesh.
+
+    :param vtk_path: The `vtk_path` parameter is a string that represents the file path to the VTK file
+    that you want to parse
+    :return: a dictionary called "temperatures" which contains the temperature values parsed from the
+    VTK file.
+    """
+    # print("Parsing ", vtk_path)
+    temperatures = {}
+    try:
+        meshio_mesh = meshio.read(vtk_path, file_format="vtk")
+    except:
+        raise Exception("Invalid vtk file")
+    for i, temp in enumerate(meshio_mesh.point_data["Temperature"]):
+        temperatures[i] = temp[0]
+    return temperatures
+
+
+def get_positions(vtk_path):
+    """
+    The function "get_positions" reads a VTK file and returns a dictionary of positions, where the keys
+    are indices and the values are corresponding positions.
+
+    :param vtk_path: The `vtk_path` parameter is the path to the VTK file that contains the mesh data
+    :return: a dictionary called "positions" which contains the positions of points in a VTK file.
+    """
+    print("Getting positions")
+    positions = {}
+    meshio_mesh = meshio.read(vtk_path, file_format="vtk")
+    for i, position in enumerate(meshio_mesh.points):
+        positions[i] = position
+    return positions
+
+
+def parse_results_vtk_series(directory, vtk_series_path, progress):
+    """
+    The function `parse_results_vtk_series` parses a VTK series file, loads the VTK files, and returns
+    the temperatures and positions from each VTK file.
+
+    :param directory: The `directory` parameter is the path to the directory where the VTK series file
+    and VTK files are located
+    :param vtk_series_path: The `vtk_series_path` parameter is the path to the vtk.series file. This
+    file contains information about a series of VTK files, including their names and corresponding time
+    steps
+    :param progress: The "progress" parameter is a callback function that is called periodically during
+    the execution of the function to provide updates on the progress of the parsing process. It takes a
+    single argument, which represents the progress as a percentage (0-100)
+    :return: The function `parse_results_vtk_series` returns two dictionaries: `results_temperatures`
+    and `results_positions`.
+    """
+    results_temperatures = {}
+    results_positions = {}
+    with open(directory + "/" + vtk_series_path) as file:
+        try:
+            vtk_series = json.load(file)
+        except:
+            raise Exception("Invalid vtk.series file")
+    print("Loaded VTK Series")
+
+    files_length = len(vtk_series["files"])
+    for i, vtk in enumerate(vtk_series["files"]):
+        progress((i / files_length) * 100)
+
+        time = vtk["time"]
+        name = vtk["name"]
+        temperatures = parse_vtk(directory + "/" + name)
+        results_temperatures[time] = temperatures
+    if len(vtk_series["files"]) > 0:
+        results_positions = get_positions(
+            directory + "/" + vtk_series["files"][0]["name"]
+        )
+    return results_temperatures, results_positions
+
+
+plt.style.use("ggplot")
+
+
+def plot_temperature_by_id(id, results):
+    """
+    Plot the temperatures of a specific node over time.
+
+    Args:
+        id (int): The ID of the node.
+        results (dict): A dictionary containing the temperature results for each time.
+
+    Returns:
+        None
+    """
+    times = []
+    temperatures = []
+    for time, result in results.items():
+        temperatures.append(result[id])
+        times.append(time)
+
+    plt.xlabel("Time", fontsize=12)
+    plt.ylabel("Temperature", fontsize=12)
+    plt.title(f"Temperatures of Node {id} Over Time", fontsize=14)
+    plt.grid(
+        True, linestyle="--", alpha=0.5
+    )  # Adding grid lines with specific style and transparency
+    plt.xticks(rotation=45)  # Rotating x-axis labels for better visibility
+    plt.tight_layout()  # Adjusting layout for better spacing
+    plt.plot(times, temperatures, label=f"ID {id}")
+    plt.show()
+
+
+def plot_all_temperatures(results):
+    """
+    Plot all temperatures over time.
+
+    Args:
+        results (dict): A dictionary containing the temperature results for each time.
+
+    Returns:
+        None
+    """
+    times = []
+    id_temperatures = {}
+
+    for time, result in results.items():
+        times.append(time)
+        for id, temp in result.items():
+            if id in id_temperatures:
+                id_temperatures[id].append(temp)
+            else:
+                id_temperatures[id] = [temp]
+
+    for id, temps in id_temperatures.items():
+        plt.plot(times, temps, linestyle="-", linewidth=2)
+
+    plt.xlabel("Time", fontsize=12)
+    plt.ylabel("Temperature", fontsize=12)
+    plt.title("All Temperatures Over Time", fontsize=14)
+    plt.grid(
+        True, linestyle="--", alpha=0.5
+    )  # Adding grid lines with specific style and transparency
+    plt.xticks(rotation=45)  # Rotating x-axis labels for better visibility
+    plt.tight_layout()  # Adjusting layout for better spacing
+    plt.show()
+
+
+def plot_average_temperature(results):
+    """
+    Plots the average temperatures over time.
+
+    Args:
+        results (dict): A dictionary containing the temperature results for each time.
+
+    Returns:
+        None
+    """
+    times = []
+    average_temperatures = []
+
+    for time, result in results.items():
+        times.append(time)
+        average_temperatures.append(sum(result.values()) / len(result.values()))
+
+    plt.plot(times, average_temperatures, linestyle="-", linewidth=2)
+    plt.xlabel("Time", fontsize=12)
+    plt.ylabel("Temperature", fontsize=12)
+    plt.title("Average Temperatures Over Time", fontsize=14)
+    plt.grid(
+        True, linestyle="--", alpha=0.5
+    )  # Adding grid lines with specific style and transparency
+    plt.xticks(rotation=45)  # Rotating x-axis labels for better visibility
+    plt.tight_layout()  # Adjusting layout for better spacing
+    plt.show()
+
+
+def plot_std_temperature(results):
+    """
+    Plots the standard deviation of temperatures over time.
+
+    Parameters:
+    results (dict): A dictionary containing time as keys and temperature values as values.
+
+    Returns:
+    None
+    """
+    times = []
+    std_temperatures = []
+
+    for time, result in results.items():
+        times.append(time)
+        std_temperatures.append(np.std(list(result.values())))
+
+    plt.plot(times, std_temperatures, linestyle="-", linewidth=2)
+    plt.xlabel("Time", fontsize=12)
+    plt.ylabel("Temperature", fontsize=12)
+    plt.title("Standard Deviation Temperatures Over Time", fontsize=14)
+    plt.grid(
+        True, linestyle="--", alpha=0.5
+    )  # Adding grid lines with specific style and transparency
+    plt.xticks(rotation=45)  # Rotating x-axis labels for better visibility
+    plt.tight_layout()  # Adjusting layout for better spacing
+    plt.show()
+
+
+def plot_3d_scatter(results_temperatures, results_positions):
+    nodes = list(results_positions.keys())
+    current_time = list(results_temperatures.keys())[0]
+
+    # Create initial plot
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection="3d")
+    scatter = ax.scatter(
+        [results_positions[node][0] for node in nodes],
+        [results_positions[node][1] for node in nodes],
+        [results_positions[node][2] for node in nodes],
+        c=[results_temperatures[current_time][node] for node in nodes],
+        cmap="viridis",
+        s=100,
+    )
+
+    # Add a color bar which maps values to colors
+    plt.grid(False)
+    cbar = plt.colorbar(scatter)
+    cbar.set_label("Temperature")
+
+    # Set labels and title
+    ax.set_xlabel("X Label")
+    ax.set_ylabel("Y Label")
+    ax.set_zlabel("Z Label")
+    ax.set_title("3D Scatter Plot with Temperature")
+
+    # Slider
+    ax_time = plt.axes([0.15, 0.02, 0.65, 0.03], facecolor="lightgoldenrodyellow")
+    slider_time = Slider(
+        ax_time,
+        "Time",
+        1,
+        len(list(results_temperatures.keys())) - 1,
+        valinit=1,
+        valstep=1,
+    )
+
+    def update(val):
+        global current_time, scatter
+        current_time = list(results_temperatures.keys())[(int(slider_time.val))]
+        ax.clear()
+        scatter = ax.scatter(
+            [results_positions[node][0] for node in nodes],
+            [results_positions[node][1] for node in nodes],
+            [results_positions[node][2] for node in nodes],
+            c=[results_temperatures[current_time][node] for node in nodes],
+            cmap="viridis",
+            s=100,
+        )
+        ax.set_xlabel("X Label")
+        ax.set_ylabel("Y Label")
+        ax.set_zlabel("Z Label")
+        ax.set_title("3D Scatter Plot with Temperature")
+        fig.canvas.draw_idle()
+
+    slider_time.on_changed(update)
+    plt.show()
+
+
+def plot_all(results_temperatures):
+    plot_all_temperatures(results_temperatures)
+    plot_temperature_by_id(0, results_temperatures)
+    plot_average_temperature(results_temperatures)
+    plot_std_temperature(results_temperatures)
+
+
+def __main__():
+    if len(sys.argv) < 3:
+        print("Usage: python script.py [directory] [file_name.vtk.series] ...")
+        sys.exit(1)
+
+    directory = sys.argv[1]
+    vtk_series_path = sys.argv[2]
+
+    results_temperatures, results_positions = parse_results_vtk_series(
+        directory, vtk_series_path
+    )
+    plot_all(results_temperatures)
+    # plot_3d_scatter(results_temperatures, results_positions)
+
+
+if __name__ == "__main__":
+    __main__()
